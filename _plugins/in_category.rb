aa9ee3b47989cdc494e886ec2baaf2a251ca4fd8
Brent Robbins <brent@variantstudios.com>
2018-08-25 14:26:52 -0600

    Fixed the blog

--- _plugins/in_category.rb
+++ _plugins/in_category.rb
@@ -1,22 +1,22 @@
 module Jekyll
-    class WithinCategoryPostNavigation < Generator
+    class WithincategoriesPostNavigation < Generator
       def generate(site)
-        site.category.each_pair do |category, posts|
+        site.categories.each_pair do |categories, posts|
           posts.sort! { |a,b| b <=> a}
           posts.each do |post|
             index = posts.index post
-            next_in_category = nil
-            previous_in_category = nil
+            next_in_categories = nil
+            previous_in_categories = nil
             if index
               if index < posts.length - 1
-                next_in_category = posts[index + 1]
+                next_in_categories = posts[index + 1]
               end
                 if index > 0
-                previous_in_category = posts[index - 1]
+                previous_in_categories = posts[index - 1]
               end
             end
-            post.data["next_in_category"] = next_in_category unless next_in_category.nil?
-            post.data["previous_in_category"] = previous_in_category unless previous_in_category.nil?
+            post.data["next_in_categories"] = next_in_categories unless next_in_categories.nil?
+            post.data["previous_in_categories"] = previous_in_categories unless previous_in_categories.nil?
           end
         end
       end
